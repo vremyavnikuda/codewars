a00d69eaeb51041e86fc85c639bf7ce8519fff64
vremayvnikuda <nevsky.andrew@gmail.com>
2022-12-14 04:42:48 +0700

    adding commit

--- src/kata_7/task_1.java
+++ src/kata_7/task_1.java
@@ -14,19 +14,30 @@ import java.util.Scanner;
 public class task_1 {
     public static void main (String []args){
         Scanner sc=new Scanner(System.in);
+        //получаем данные которые войдут в условие заполнения массива
+        //а==первое число
         int a= sc.nextInt();
+        //b==второе число
         int b= sc.nextInt();
+        //так как нам не известна длинна массива
+        //создаем счетчик длинны
         int count=0;
+        //задаем временную переменную в которую сложим значение переменной a
         int q=a;
+        //задаем цикл (при помощи которого будем искать длину массава)
         while (q<=b){
             q++;
             count++;
         }
+        //инициализируем массив
         int []between=new int[count];
+        //создаем условие заполнения массива данными
         for (int i=0;i< between.length;i++){
             between[i]+=a++;
         }
+        //создаем условие вывода массива данных
         for (int i=0;i< between.length;i++) {
+            //через пробел
             System.out.print(between[i] + " ");
         }
     }
