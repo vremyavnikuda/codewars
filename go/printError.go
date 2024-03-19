3a33cc5919249de1039dd156ec6a61bc7bebd855
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-03-19 21:50:19 +0700

    add task printerError

---
+++ go/printError.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func PrinterError(s string) string {
+	errorCount := 0
+
+	total := len(s)
+
+	for _, char := range s {
+		if char < 'a' || char > 'm' {
+			errorCount++
+		}
+	}
+
+	return fmt.Sprintf("%d/%d", errorCount, total)
+}
