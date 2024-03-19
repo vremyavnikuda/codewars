ef03710027b470d6c18dbe555dfd2ff664e693dd
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-03-19 17:54:18 +0700

    Divisors -> кол-во делителей натурального числа

---
+++ go/sum_numbers_divisors.go
@@ -0,0 +1,17 @@
+package main
+
+//Посчитайте колко делителей натурального числа
+func Divisors(n int) int {
+
+	//счеткик делителей натурального числа
+	sum := 0
+	if n == 0 {
+		return 0
+	}
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			sum++
+		}
+	}
+	return sum
+}
