3e0dc4833ea552af2388fa5185e0144dea30654a
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-06-19 16:50:40 +0700

    task offer
    Дано натуральное число N. Выведите его представление в двоичном виде.

---
+++ go/binaryRecords.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var n int
+	fmt.Print("Enter n: ")
+	fmt.Scanln(&n)
+	
+	binary_representation := strconv.FormatInt(int64(n), 2)
+
+	fmt.Println(binary_representation)
+}
