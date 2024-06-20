d8b1b65bd5707764dec7845d05b053c708afa744
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-06-20 17:44:39 +0700

    add offer sapmle code golang

---
+++ go/funcStruct.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	x, y, r float64
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.x * c.y * c.r
+}
+
+func main() {
+	c := Circle{1.1, 1.2, 1.3}
+	fmt.Println(c.area())
+	r := Rectangle{0, 0, 10, 10}
+	fmt.Println(r.area1())
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+func (r *Rectangle) area1() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
