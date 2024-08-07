9cd89fc0be803218a2ca12b96010f7d9889452e6
Andrew <andrew.nevsky@yandex.ru>
2024-08-07 23:04:53 +0700

    Update task1.go

--- go/editTypeData/task1.go
+++ go/editTypeData/task1.go
@@ -19,11 +19,9 @@ func main() {
 	s = strings.ReplaceAll(s, ",", ".")
 	// Делим строку по разделителю
 	arr := strings.Split(s, ";")
-
 	if len(arr) != 2 {
 		return
 	}
-
 	// Конвертируем строчные значения в числа с плавающей точкой
 	number1, err1 := strconv.ParseFloat(arr[0], 64)
 	number2, err2 := strconv.ParseFloat(arr[1], 64)
@@ -31,12 +29,10 @@ func main() {
 		fmt.Println(err1, err2)
 		return
 	}
-
 	// Выполняем операцию и выводим результат
 	if number1 == 0 {
 		return
 	}
-
 	result := number1 / number2
 	fmt.Printf("%.4f\n", result)
 }
