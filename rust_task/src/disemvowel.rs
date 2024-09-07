c7eb61ca58cb96aaa79c51cd3c0fc8035c7ebf9e
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-09-08 01:36:55 +0700

    add

--- rust_task/src/disemvowel.rs
+++ rust_task/src/disemvowel.rs
@@ -14,7 +14,8 @@ fn disemvowel(s: &str) -> String {
 #[allow(unused)]
 fn main() {
     println!("{}", disemvowel("hello"));
-    println!("{}", double_char("hello"))
+    println!("{}", double_char("hello"));
+    println!("{}", fake_bin("8346573645"));
 }
 //Дублируем все буквы в словах
 fn double_char(s: &str) -> String {
@@ -27,4 +28,16 @@ fn double_char(s: &str) -> String {
         }
     }
     _result
+}
+fn fake_bin(s: &str) -> String {
+    s
+        .chars()
+        .map(|ch| match ch.to_digit(10) {
+            Some(digit) => match digit {
+                d if d <= 5 => '0',
+                _ => '1',
+            },
+            None => ch,
+        })
+        .collect()
 }
