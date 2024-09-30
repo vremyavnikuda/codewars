6f4b611a03cb0f24bd38e8c368030a58b77fe8e1
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-09-30 23:25:59 +0700

    +

---
+++ go/golang/arrays.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+func main(){
+	arrays()
+	fmt.Println("-----------")
+	initArray()
+}
+
+func arrays(){
+	a:=[...]int{1,2,3};
+	b:=[3]int{3,4,5}
+	println(a)
+	println(b)
+}
+
+func println(a [3]int){
+	for idx,elem := range a{
+		//idx -> индекс массива
+		//elm -> элемент массива
+		fmt.Println(idx,"->", elem)
+	}
+}
+func initArray(){
+	var a [3]int = [3]int{1,2,3}
+	b:=[3]int{1,2,3}
+	c:=[...]int{1,2,3}
+	d:=[3]int{1:12}
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d==c)
+}
