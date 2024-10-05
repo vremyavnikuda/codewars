c2b3f3531d21bccd14e25797e1a63156a0b14f4b
vremyavnikuda <andrew.nevsky@yandex.ru>
2024-10-05 15:58:06 +0700

    add

--- go/golang/arrays.go
+++ go/golang/arrays.go
@@ -1,33 +1,34 @@
 package main
 
 import "fmt"
-func main(){
+
+func main() {
 	arrays()
 	fmt.Println("-----------")
 	initArray()
 }
 
-func arrays(){
-	a:=[...]int{1,2,3};
-	b:=[3]int{3,4,5}
+func arrays() {
+	a := [...]int{1, 2, 3}
+	b := [3]int{3, 4, 5}
 	println(a)
 	println(b)
 }
 
-func println(a [3]int){
-	for idx,elem := range a{
+func println(a [3]int) {
+	for idx, elem := range a {
 		//idx -> индекс массива
 		//elm -> элемент массива
-		fmt.Println(idx,"->", elem)
+		fmt.Println(idx, "->", elem)
 	}
 }
-func initArray(){
-	var a [3]int = [3]int{1,2,3}
-	b:=[3]int{1,2,3}
-	c:=[...]int{1,2,3}
-	d:=[3]int{1:12}
+func initArray() {
+	var a [3]int = [3]int{1, 2, 3}
+	b := [3]int{1, 2, 3}
+	c := [...]int{1, 2, 3}
+	d := [3]int{1: 12}
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Println(d==c)
+	fmt.Println(d == c)
 }
--- go/golang/func/sampleCode.go
+++ go/golang/func/sampleCode.go
@@ -2,12 +2,37 @@ package main
 
 import "fmt"
 
-func main(){
-	MyPrint(1,2,3,4,5)
+func main() {
+	MyPrint(1, 2, 3, 4, 5)
+	x := 5
+	zero(&x)
+	fmt.Println(x)
+	fmt.Println("====================")
+	i := 11
+	changer(&i)
+	fmt.Println(i)
 }
 
-func MyPrint(a...interface{}){
-	for _,elem := range a{
-		fmt.Printf("%d",elem)
+func MyPrint(a ...interface{}) {
+	for _, elem := range a {
+		fmt.Printf("%d", elem)
 	}
+	fmt.Println("---------------------")
+}
+func zero(x *int) {
+	*x = 100
+}
+func changer(i *int) {
+	*i = 2 * *i
+}
+
+// считайте что fmt уже импортирован и main объявлен
+func test(x1 *int, x2 *int) {
+	i := *x1 * *x2
+	fmt.Println(i)
+}
+
+func remove(x1 *int, x2 *int) {
+	*x1, *x2 = *x2, *x1
+	fmt.Println(*x1, *x2)
 }
