7b5cd47e3189979a3751a29b0a7f6d954846e7ee
vremyavnikuda <andrew.nevsky@yandex.ru>
2025-02-16 22:37:04 +0700

    add new task

--- rust/rust_task/src/main.rs
+++ rust/rust_task/src/main.rs
@@ -12,8 +12,8 @@ fn sum_number() {
     let mut number1 = String::new();
     let mut number2 = String::new();
 
-    io::stdin().read_line(&mut number1);
-    io::stdin().read_line(&mut number2);
+    io::stdin().read_line(&mut number1).expect("Error");
+    io::stdin().read_line(&mut number2).expect("Error");
 
     let number1: i32 = number1.trim().parse().unwrap();
     let number2: i32 = number2.trim().parse().unwrap();
