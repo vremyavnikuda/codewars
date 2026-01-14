b507b9a67f111ebf6099dbe1b6c116afe77fdb13
vremyavnikuda <andrew.nevsky@yandex.ru>
2026-01-14 19:18:03 +0700

    refactor(BST): add

--- c++/algo_and_data/kr_1/BST.cpp
+++ c++/algo_and_data/kr_1/BST.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-void ShowMenu() {
+void ShowMenu()
+{
     cout << "\n МЕНЮ: " << endl;
     cout << "1. Вставка элемента" << endl;
     cout << "2. Удаление элемента" << endl;
@@ -19,11 +20,13 @@ void ShowMenu() {
     cout << "0. Выход" << endl;
 }
 
-void IteratorMenu(BST<int, string> *tree) {
+void IteratorMenu(BST<int, string> *tree)
+{
     BST<int, string>::Iterator it(tree);
     bool exit = false;
 
-    while (!exit) {
+    while (!exit)
+    {
         cout << "\n Меню итератора" << endl;
         cout << "1. Установить на начало" << endl;
         cout << "2. Переход к следующему" << endl;
@@ -32,157 +35,177 @@ void IteratorMenu(BST<int, string> *tree) {
         cout << "5. Оператор *" << endl;
         cout << "6. Оператор ++" << endl;
         cout << "0. Выход из итератора" << endl;
-
         int choice;
         cin >> choice;
+        switch (choice)
+        {
+        case 1:
+            it.Begin();
+            cout << "Итератор установлен на начало" << endl;
+            break;
 
-        switch (choice) {
-            case 1:
-                it.Begin();
-                cout << "Итератор установлен на начало" << endl;
-                break;
+        case 2:
+            it.Next();
+            cout << "Переход к следующему" << endl;
+            break;
 
-            case 2:
-                it.Next();
-                cout << "Переход к следующему" << endl;
-                break;
+        case 3:
+            cout << it.IsOff() << endl;
+            break;
 
-            case 3:
-                cout << it.IsOff() << endl;
-                break;
+        case 4:
+            try
+            {
+                cout << *it << endl;
+            }
+            catch (BSTException &e)
+            {
+                cout << e.What() << endl;
+            }
+            break;
 
-            case 4:
-                try {
-                    cout << *it << endl;
-                } catch (BSTException &e) {
-                    cout << e.What() << endl;
-                }
-                break;
+        case 5:
+            try
+            {
+                cout << *it << endl;
+            }
+            catch (BSTException &e)
+            {
+                cout << e.What() << endl;
+            }
+            break;
 
-            case 5:
-                try {
-                    cout << *it << endl;
-                } catch (BSTException &e) {
-                    cout << e.What() << endl;
-                }
-                break;
+        case 6:
+            ++it;
+            cout << "Оператор ++ выполнен" << endl;
+            break;
 
-            case 6:
-                ++it;
-                cout << "Оператор ++ выполнен" << endl;
-                break;
+        case 0:
+            exit = true;
+            break;
 
-            case 0:
-                exit = true;
-                break;
-
-            default:
-                cout << "Неверный выбор" << endl;
+        default:
+            cout << "Неверный выбор" << endl;
         }
     }
 }
 
-int main() {
+int main()
+{
     system("chcp 65001 > nul");
     setlocale(LC_ALL, "ru_RU.UTF-8");
     // Создание дерева с ключами типа int и данными типа string
     BST<int, string> tree;
-
     ShowMenu();
-
     bool exit = false;
-    while (!exit) {
+    while (!exit)
+    {
         cout << "\nВведите номер операции: ";
         int choice;
         cin >> choice;
+        switch (choice)
+        {
+        // Вставка
+        case 1:
+        {
+            int key;
+            string data;
+            cout << "Введите ключ: ";
+            cin >> key;
+            cout << "Введите данные: ";
+            cin >> data;
+            bool result = tree.Insert(key, data);
+            cout << result << endl;
+            break;
+        }
+        // Удаление
+        case 2:
+        {
+            int key;
+            cout << "Введите ключ для удаления: ";
+            cin >> key;
+            bool result = tree.Delete(key);
+            cout << result << endl;
+            break;
+        }
+        // Поиск
+        case 3:
+        {
+            int key;
+            cout << "Введите ключ для поиска: ";
+            cin >> key;
 
-        switch (choice) {
-            // Вставка
-            case 1: {
-                int key;
-                string data;
-                cout << "Введите ключ: ";
-                cin >> key;
-                cout << "Введите данные: ";
-                cin >> data;
-                bool result = tree.Insert(key, data);
-                cout << result << endl;
-                break;
+            try
+            {
+                string data = tree.Search(key);
+                cout << data << endl;
             }
-            // Удаление
-            case 2: {
-                int key;
-                cout << "Введите ключ для удаления: ";
-                cin >> key;
-                bool result = tree.Delete(key);
-                cout << result << endl;
-                break;
+            catch (BSTException &e)
+            {
+                cout << e.What() << endl;
             }
-            // Поиск
-            case 3: {
-                int key;
-                cout << "Введите ключ для поиска: ";
-                cin >> key;
-
-                try {
-                    string data = tree.Search(key);
-                    cout << data << endl;
-                } catch (BSTException &e) {
-                    cout << e.What() << endl;
-                }
-                break;
-            }
-            // Обход Lt → t → Rt
-            case 4: {
-                tree.Traverse();
-                break;
-            }
-            // Вывод структуры
-            case 5: {
-                tree.Show();
-                break;
-            }
-            // Размер
-            case 6: {
-                cout << tree.Size() << endl;
-                break;
-            }
-            // Проверка на пустоту
-            case 7: {
-                cout << tree.IsEmpty() << endl;
-                break;
-            }
-            // Очистка
-            case 8: {
-                tree.Clear();
-                cout << "Дерево очищено" << endl;
-                break;
-            }
-            // Формирование критериев сбалансированности
-            case 9: {
-                tree.UpdateBalance();
-                cout << "Критерии сбалансированности обновлены" << endl;
-                cout << "Структура дерева с критериями:" << endl;
-                tree.Show();
-                break;
-            }
-            // Итератор
-            case 10: {
-                IteratorMenu(&tree);
-                break;
-            }
-            // Показать меню
-            case 11: {
-                ShowMenu();
-                break;
-            }
-            // Выход
-            case 0: {
-                exit = true;
-                break;
-            }
-            default:
-                cout << "Неверный выбор" << endl;
+            break;
+        }
+        // Обход Lt → t → Rt
+        case 4:
+        {
+            tree.Traverse();
+            break;
+        }
+        // Вывод структуры
+        case 5:
+        {
+            tree.Show();
+            break;
+        }
+        // Размер
+        case 6:
+        {
+            cout << tree.Size() << endl;
+            break;
+        }
+        // Проверка на пустоту
+        case 7:
+        {
+            cout << tree.IsEmpty() << endl;
+            break;
+        }
+        // Очистка
+        case 8:
+        {
+            tree.Clear();
+            cout << "Дерево очищено" << endl;
+            break;
+        }
+        // Формирование критериев сбалансированности
+        case 9:
+        {
+            tree.UpdateBalance();
+            cout << "Критерии сбалансированности обновлены" << endl;
+            cout << "Структура дерева с критериями:" << endl;
+            tree.Show();
+            break;
+        }
+        // Итератор
+        case 10:
+        {
+            IteratorMenu(&tree);
+            break;
+        }
+        // Показать меню
+        case 11:
+        {
+            ShowMenu();
+            break;
+        }
+        // Выход
+        case 0:
+        {
+            exit = true;
+            break;
+        }
+        default:
+            cout << "Неверный выбор" << endl;
         }
     }
     return 0;
--- c++/algo_and_data/kr_1/BST.h
+++ c++/algo_and_data/kr_1/BST.h
@@ -5,20 +5,23 @@
 using namespace std;
 
 // Класс исключений для BST-дерева
-class BSTException {
+class BSTException
+{
   string message;
 
 public:
   BSTException(string msg) : message(msg) {}
-
   string What() { return message; }
 };
 
 // Шаблонный класс BST-дерево
-template <class TKey, class TData> class BST {
+template <class TKey, class TData>
+class BST
+{
 protected:
   // Внутренний класс для узла дерева
-  class Node {
+  class Node
+  {
   public:
     TKey key;
     TData data;
@@ -26,111 +29,83 @@ protected:
     Node *right;
     // Критерий сбалансированности
     int balance;
-
     Node(TKey k, TData d)
         : key(k), data(d), left(nullptr), right(nullptr), balance(0) {}
   };
-
   Node *root;
   int count;
-
   // Вспомогательные рекурсивные методы
   Node *InsertRec(Node *t, TKey k, TData data, bool &inserted);
-
   Node *DeleteRec(Node *t, TKey k, bool &deleted);
-
   Node *Del(Node *t, Node *t0);
-
   TData SearchRec(Node *t, TKey k);
-
   void ClearRec(Node *t);
-
   void TraverseInOrder(Node *t);
-
   void ShowRec(Node *t, int level);
-
   int CalcHeight(Node *t);
-
   void CalcBalance(Node *t);
-
   Node *CopyRec(Node *t);
 
 public:
   BST();
-
   BST(const BST<TKey, TData> &tree);
-
   ~BST();
-
   // Основные операции АТД
   int Size() const { return count; }
-
   void Clear();
-
   bool IsEmpty() const { return count == 0; }
-
   TData Search(TKey k);
-
   bool Insert(TKey k, TData data);
-
   bool Delete(TKey k);
-
   void Traverse();
-
   void Show();
-
   // Дополнительная операция
   void UpdateBalance();
-
   // Внутренний класс итератора
-  class Iterator {
+  class Iterator
+  {
   private:
     BST<TKey, TData> *owner;
     Node **stack;
     int stackSize;
     int top;
     Node *current;
-
     void PushLeftPath(Node *node);
 
   public:
     Iterator(BST<TKey, TData> *owner);
-
     ~Iterator();
-
     void Begin();
-
     void Next();
-
     bool IsOff();
-
     TData &operator*();
-
     bool operator==(const Iterator &other);
-
     Iterator &operator++();
   };
 };
 
 // Конструктор
-template <class TKey, class TData> BST<TKey, TData>::BST() {
+template <class TKey, class TData>
+BST<TKey, TData>::BST()
+{
   root = nullptr;
   count = 0;
 }
 
 // Конструктор копирования
 template <class TKey, class TData>
-BST<TKey, TData>::BST(const BST<TKey, TData> &tree) {
+BST<TKey, TData>::BST(const BST<TKey, TData> &tree)
+{
   root = CopyRec(tree.root);
   count = tree.count;
 }
 
 // Рекурсивное копирование дерева
 template <class TKey, class TData>
-typename BST<TKey, TData>::Node *BST<TKey, TData>::CopyRec(Node *t) {
+typename BST<TKey, TData>::Node *BST<TKey, TData>::CopyRec(Node *t)
+{
   if (t == nullptr)
     return nullptr;
-
   Node *newNode = new Node(t->key, t->data);
   newNode->balance = t->balance;
   newNode->left = CopyRec(t->left);
@@ -140,16 +115,21 @@ typename BST<TKey, TData>::Node *BST<TKey, TData>::CopyRec(Node *t) {
 }
 
 // Деструктор
-template <class TKey, class TData> BST<TKey, TData>::~BST() { Clear(); }
+template <class TKey, class TData>
+BST<TKey, TData>::~BST() { Clear(); }
 
 // Очистка дерева
-template <class TKey, class TData> void BST<TKey, TData>::Clear() {
+template <class TKey, class TData>
+void BST<TKey, TData>::Clear()
+{
   ClearRec(root);
   root = nullptr;
   count = 0;
 }
 
-template <class TKey, class TData> void BST<TKey, TData>::ClearRec(Node *t) {
+template <class TKey, class TData>
+void BST<TKey, TData>::ClearRec(Node *t)
+{
   if (t == nullptr)
     return;
   ClearRec(t->left);
@@ -159,7 +139,8 @@ template <class TKey, class TData> void BST<TKey, TData>::ClearRec(Node *t) {
 
 // Рекурсивная вставка
 template <class TKey, class TData>
-bool BST<TKey, TData>::Insert(TKey k, TData data) {
+bool BST<TKey, TData>::Insert(TKey k, TData data)
+{
   bool inserted = false;
   root = InsertRec(root, k, data, inserted);
   if (inserted)
@@ -169,38 +150,39 @@ bool BST<TKey, TData>::Insert(TKey k, TData data) {
 
 template <class TKey, class TData>
 typename BST<TKey, TData>::Node *
-BST<TKey, TData>::InsertRec(Node *t, TKey k, TData data, bool &inserted) {
-  if (t == nullptr) {
+BST<TKey, TData>::InsertRec(Node *t, TKey k, TData data, bool &inserted)
+{
+  if (t == nullptr)
+  {
     inserted = true;
     return new Node(k, data);
   }
-
-  if (k == t->key) {
+  if (k == t->key)
+  {
     inserted = false;
     return t;
   }
-
   if (k < t->key)
     t->left = InsertRec(t->left, k, data, inserted);
   else
     t->right = InsertRec(t->right, k, data, inserted);
-
   return t;
 }
 
 // Рекурсивный поиск
-template <class TKey, class TData> TData BST<TKey, TData>::Search(TKey k) {
+template <class TKey, class TData>
+TData BST<TKey, TData>::Search(TKey k)
+{
   return SearchRec(root, k);
 }
 
 template <class TKey, class TData>
-TData BST<TKey, TData>::SearchRec(Node *t, TKey k) {
+TData BST<TKey, TData>::SearchRec(Node *t, TKey k)
+{
   if (t == nullptr)
     throw BSTException("Ключ не найден");
-
   if (k == t->key)
     return t->data;
-
   if (k < t->key)
     return SearchRec(t->left, k);
   else
@@ -208,7 +190,9 @@ TData BST<TKey, TData>::SearchRec(Node *t, TKey k) {
 }
 
 // Рекурсивное удаление
-template <class TKey, class TData> bool BST<TKey, TData>::Delete(TKey k) {
+template <class TKey, class TData>
+bool BST<TKey, TData>::Delete(TKey k)
+{
   bool deleted = false;
   root = DeleteRec(root, k, deleted);
   if (deleted)
@@ -218,52 +202,53 @@ template <class TKey, class TData> bool BST<TKey, TData>::Delete(TKey k) {
 
 template <class TKey, class TData>
 typename BST<TKey, TData>::Node *BST<TKey, TData>::DeleteRec(Node *t, TKey k,
-                                                             bool &deleted) {
-  if (t == nullptr) {
+                                                             bool &deleted)
+{
+  if (t == nullptr)
+  {
     deleted = false;
     return t;
   }
-
-  if (k < t->key) {
+  if (k < t->key)
+  {
     t->left = DeleteRec(t->left, k, deleted);
     return t;
   }
-
-  if (k > t->key) {
+  if (k > t->key)
+  {
     t->right = DeleteRec(t->right, k, deleted);
     return t;
   }
-
   deleted = true;
-
-  if (t->left == nullptr && t->right == nullptr) {
+  if (t->left == nullptr && t->right == nullptr)
+  {
     delete t;
     return nullptr;
   }
-
-  if (t->left == nullptr) {
+  if (t->left == nullptr)
+  {
     Node *x = t->right;
     delete t;
     return x;
   }
-
-  if (t->right == nullptr) {
+  if (t->right == nullptr)
+  {
     Node *x = t->left;
     delete t;
     return x;
   }
-
   t->right = Del(t->right, t);
   return t;
 }
 
 template <class TKey, class TData>
-typename BST<TKey, TData>::Node *BST<TKey, TData>::Del(Node *t, Node *t0) {
-  if (t->left != nullptr) {
+typename BST<TKey, TData>::Node *BST<TKey, TData>::Del(Node *t, Node *t0)
+{
+  if (t->left != nullptr)
+  {
     t->left = Del(t->left, t0);
     return t;
   }
-
   t0->key = t->key;
   t0->data = t->data;
   Node *x = t->right;
@@ -272,13 +257,16 @@ typename BST<TKey, TData>::Node *BST<TKey, TData>::Del(Node *t, Node *t0) {
 }
 
 // Обход Lt → t → Rt (симметричный)
-template <class TKey, class TData> void BST<TKey, TData>::Traverse() {
+template <class TKey, class TData>
+void BST<TKey, TData>::Traverse()
+{
   TraverseInOrder(root);
   cout << endl;
 }
 
 template <class TKey, class TData>
-void BST<TKey, TData>::TraverseInOrder(Node *t) {
+void BST<TKey, TData>::TraverseInOrder(Node *t)
+{
   if (t == nullptr)
     return;
   TraverseInOrder(t->left);
@@ -287,38 +275,43 @@ void BST<TKey, TData>::TraverseInOrder(Node *t) {
 }
 
 // Вычисление высоты поддерева
-template <class TKey, class TData> int BST<TKey, TData>::CalcHeight(Node *t) {
+template <class TKey, class TData>
+int BST<TKey, TData>::CalcHeight(Node *t)
+{
   if (t == nullptr)
     return 0;
 
   int leftHeight = CalcHeight(t->left);
   int rightHeight = CalcHeight(t->right);
-
   return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
 }
 
 // Вычисление критерия сбалансированности
-template <class TKey, class TData> void BST<TKey, TData>::CalcBalance(Node *t) {
+template <class TKey, class TData>
+void BST<TKey, TData>::CalcBalance(Node *t)
+{
   if (t == nullptr)
     return;
-
   CalcBalance(t->left);
   CalcBalance(t->right);
-
   int leftHeight = CalcHeight(t->left);
   int rightHeight = CalcHeight(t->right);
-
   t->balance = rightHeight - leftHeight;
 }
 
 // Дополнительная операция: формирование критериев сбалансированности
-template <class TKey, class TData> void BST<TKey, TData>::UpdateBalance() {
+template <class TKey, class TData>
+void BST<TKey, TData>::UpdateBalance()
+{
   CalcBalance(root);
 }
 
 // Вывод структуры дерева
-template <class TKey, class TData> void BST<TKey, TData>::Show() {
-  if (root == nullptr) {
+template <class TKey, class TData>
+void BST<TKey, TData>::Show()
+{
+  if (root == nullptr)
+  {
     cout << "Дерево пусто" << endl;
     return;
   }
@@ -326,23 +319,21 @@ template <class TKey, class TData> void BST<TKey, TData>::Show() {
 }
 
 template <class TKey, class TData>
-void BST<TKey, TData>::ShowRec(Node *t, int level) {
+void BST<TKey, TData>::ShowRec(Node *t, int level)
+{
   if (t == nullptr)
     return;
-
   ShowRec(t->right, level + 1);
-
   for (int i = 0; i < 3 * level; i++)
     cout << " ";
-
-  cout << t->key << " (" << t->balance << ")" << endl;
-
+  cout << t->data << endl;
   ShowRec(t->left, level + 1);
 }
 
 // Реализация итератора
 template <class TKey, class TData>
-BST<TKey, TData>::Iterator::Iterator(BST<TKey, TData> *owner) {
+BST<TKey, TData>::Iterator::Iterator(BST<TKey, TData> *owner)
+{
   this->owner = owner;
   stackSize = owner->count + 1;
   stack = new Node *[stackSize];
@@ -350,63 +341,81 @@ BST<TKey, TData>::Iterator::Iterator(BST<TKey, TData> *owner) {
   current = nullptr;
 }
 
-template <class TKey, class TData> BST<TKey, TData>::Iterator::~Iterator() {
+template <class TKey, class TData>
+BST<TKey, TData>::Iterator::~Iterator()
+{
   delete[] stack;
 }
 
 template <class TKey, class TData>
-void BST<TKey, TData>::Iterator::PushLeftPath(Node *node) {
-  while (node != nullptr) {
+void BST<TKey, TData>::Iterator::PushLeftPath(Node *node)
+{
+  while (node != nullptr)
+  {
     stack[++top] = node;
     node = node->left;
   }
 }
 
-template <class TKey, class TData> void BST<TKey, TData>::Iterator::Begin() {
+template <class TKey, class TData>
+void BST<TKey, TData>::Iterator::Begin()
+{
   top = -1;
   current = nullptr;
-  if (owner->root != nullptr) {
+  if (owner->root != nullptr)
+  {
     PushLeftPath(owner->root);
-    if (top >= 0) {
+    if (top >= 0)
+    {
       current = stack[top];
       top--;
     }
   }
 }
 
-template <class TKey, class TData> void BST<TKey, TData>::Iterator::Next() {
+template <class TKey, class TData>
+void BST<TKey, TData>::Iterator::Next()
+{
   if (current == nullptr)
     return;
 
   if (current->right != nullptr)
     PushLeftPath(current->right);
 
-  if (top >= 0) {
+  if (top >= 0)
+  {
     current = stack[top];
     top--;
-  } else {
+  }
+  else
+  {
     current = nullptr;
   }
 }
 
-template <class TKey, class TData> bool BST<TKey, TData>::Iterator::IsOff() {
+template <class TKey, class TData>
+bool BST<TKey, TData>::Iterator::IsOff()
+{
   return current == nullptr;
 }
 
 template <class TKey, class TData>
-TData &BST<TKey, TData>::Iterator::operator*() {
+TData &BST<TKey, TData>::Iterator::operator*()
+{
   if (current == nullptr)
     throw BSTException("Итератор не установлен");
   return current->data;
 }
 
 template <class TKey, class TData>
-bool BST<TKey, TData>::Iterator::operator==(const Iterator &other) {
+bool BST<TKey, TData>::Iterator::operator==(const Iterator &other)
+{
   return current == other.current;
 }
 
 template <class TKey, class TData>
-typename BST<TKey, TData>::Iterator &BST<TKey, TData>::Iterator::operator++() {
+typename BST<TKey, TData>::Iterator &BST<TKey, TData>::Iterator::operator++()
+{
   Next();
   return *this;
 }
