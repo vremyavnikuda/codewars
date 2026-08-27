f6e55b53ee8d89766fc5fd79b8cfc795e56f7b20
Andrew <andrew.nevsky@yandex.ru>
2026-08-28 02:37:06 +0700

    feat: add rust solution to lc problem: No.3720

---
+++ rust/leetcode/src/q2/q2_lex_greater_permutation.rs
@@ -0,0 +1,46 @@
+impl Solution {
+    pub fn lex_greater_permutation(s: String, target: String) -> String {
+        let mut aa = s.into_bytes();
+        let bb = target.as_bytes();
+        let mut cc = [0usize; 26];
+
+        for &dd in &aa {
+            cc[(dd - b'a') as usize] += 1;
+        }
+
+        let mut dd = 0;
+        while dd < bb.len() {
+            let ee = (bb[dd] - b'a') as usize;
+            if cc[ee] == 0 {
+                break;
+            }
+            aa[dd] = bb[dd];
+            cc[ee] -= 1;
+            dd += 1;
+        }
+
+        loop {
+            if dd < bb.len() {
+                let ee = (bb[dd] - b'a') as usize + 1;
+                if let Some(ff) = (ee..26).find(|&gg| cc[gg] > 0) {
+                    aa[dd] = b'a' + ff as u8;
+                    cc[ff] -= 1;
+                    let mut ee = dd + 1;
+                    for (ff, &gg) in cc.iter().enumerate() {
+                        for _ in 0..gg {
+                            aa[ee] = b'a' + ff as u8;
+                            ee += 1;
+                        }
+                    }
+                    return String::from_utf8(aa).unwrap();
+                }
+            }
+
+            if dd == 0 {
+                return String::new();
+            }
+            dd -= 1;
+            cc[(bb[dd] - b'a') as usize] += 1;
+        }
+    }
+}
